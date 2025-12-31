77ed48a79c895cf181707c11c92d628032177239
w.jamesdevera <w.jamesdevera@gmail.com>
2026-01-01 00:46:15 +0800

    feat: add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,23 @@ export default function Home() {
     <div className={s.page_layout}>
       <Navbar />
       <main className={s.main}>
-        <div className={s.wrapper}></div>
+        <div className={s.wrapper}>
+          <section className={`${s.hero_section} ${s.section}`}>
+            <div className={s.hero}>
+              <h1 className={s.title}>Winfrey De Vera</h1>
+              <p className={s.sub_title}>
+                Designing the Engine Behind the Interface.
+              </p>
+              <p className={s.description}>
+                I am a Software Engineer based in Quezon City, dedicated to
+                building high-performance web applications. I thrive at the
+                intersection of clean code and impactful community-driven
+                technology
+              </p>
+            </div>
+          </section>
+          <section>experience</section>
+        </div>
       </main>
       <Footer />
     </div>
