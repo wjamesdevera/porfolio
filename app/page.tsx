28959ba28cd816aaa659b70698ba285e1a35e0e0
w.jamesdevera <w.jamesdevera@gmail.com>
2026-01-11 23:18:27 +0800

    add about me page

--- app/page.tsx
+++ app/page.tsx
@@ -21,7 +21,43 @@ export default function Home() {
               </p>
             </div>
           </section>
-          <section>experience</section>
+          <section className={`${s.section} ${s.about_me}`}>
+            <div className={s.wrapper}>
+              <h2 className={s.title}>about me</h2>
+              <p>
+                With over two years of experience in the digital space, I
+                currently serve as the Lead Frontend Developer at TomasinoWeb.
+                In this role, I bridge the gap between creative design and
+                technical execution, mentoring a team of developers to bring
+                ambitious digital visions to life.
+              </p>
+              <p>
+                Beyond professional development, I am passionate about
+                knowledge-sharing. I’ve spent my free time tutoring fellow
+                college students in programming, helping them navigate the
+                complexities of modern software development.
+              </p>
+              <p>Here are some technologies I have been working on:</p>
+              <div className={s.skill_list}>
+                <ul className={s.left}>
+                  <li>python</li>
+                  <li>react.js</li>
+                  <li>javascrip es6+</li>
+                </ul>
+                <ul className={s.right}>
+                  <li>typescript</li>
+                  <li>java</li>
+                  <li>php</li>
+                </ul>
+              </div>
+              <p>
+                When I'm not at my desk, you'll find me exploring the latest
+                tech trends, and curated playlists. I believe the discipline of
+                working out mirrors the persistence needed in coding: progress
+                is a result of consistency.
+              </p>
+            </div>
+          </section>
         </div>
       </main>
       <Footer />
