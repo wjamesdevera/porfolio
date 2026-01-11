a1ce9fd86332a7acfc90feca67cefcab6410ee91
w.jamesdevera <w.jamesdevera@gmail.com>
2026-01-11 23:49:26 +0800

    add experience section

--- app/page.tsx
+++ app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
       <Navbar />
       <main className={s.main}>
         <div className={s.wrapper}>
-          <section className={`${s.hero_section} ${s.section}`}>
+          <article className={`${s.hero_section} ${s.section}`}>
             <div className={s.hero}>
               <h1 className={s.title}>Winfrey De Vera</h1>
               <p className={s.sub_title}>
@@ -20,8 +20,9 @@ export default function Home() {
                 technology
               </p>
             </div>
-          </section>
-          <section className={`${s.section} ${s.about_me}`}>
+          </article>
+
+          <article className={`${s.section} ${s.about_me} ${s.border_bottom}`}>
             <div className={s.wrapper}>
               <h2 className={s.title}>about me</h2>
               <p>
@@ -57,7 +58,90 @@ export default function Home() {
                 is a result of consistency.
               </p>
             </div>
-          </section>
+          </article>
+
+          <article
+            className={`${s.section} ${s.experience} ${s.border_bottom}`}
+          >
+            <div className={s.wrapper}>
+              <h2 className={s.title}>experience</h2>
+              <div className={s.content}>
+                <div className={s.experience_container}>
+                  <div className="">
+                    <h3 className={s.title}>TomasinoWeb</h3>
+                  </div>
+                  <div className={s.description}>
+                    <p className={s.role}>
+                      lead frontend Developer @tomasinoweb
+                    </p>
+                    <p className={s.date}>jul 2025 - present</p>
+                    <p className={s.task}>
+                      Lead UI development for multiple web projects, mentoring
+                      junior developers, and overseeing frontend code quality.
+                    </p>
+                    <p className={s.task}>
+                      Collaborated with backend and design teams to improve
+                      accessibility, performance, and overall user experience.
+                    </p>
+                    <p className={s.task}>
+                      Championed Git-based collaboration workflows and conducted
+                      code reviews for maintainable releases
+                    </p>
+                  </div>
+                </div>
+
+                <div className={s.experience_container}>
+                  <div className="">
+                    <h3 className={s.title}>TomasinoWeb</h3>
+                  </div>
+                  <div className={s.description}>
+                    <p className={s.role}>frontend Developer @tomasinoweb</p>
+                    <p className={s.date}>jul 2023 - jul 2025</p>
+                    <p className={s.task}>
+                      Contributed to Lamona and Thomscore, university
+                      publication platforms
+                    </p>
+                    <p className={s.task}>
+                      Developed responsive and accessible UI components,
+                      improving design consistency and load efficiency
+                    </p>
+                    <p className={s.task}>
+                      Enhanced cross-functional collaboration by aligning
+                      frontend and backend integration.
+                    </p>
+                  </div>
+                </div>
+
+                <div className={s.experience_container}>
+                  <div className="">
+                    <h3 className={s.title}>TwinCJ Glamping Resort</h3>
+                  </div>
+                  <div className={s.description}>
+                    <p className={s.role}>
+                      system analyst @twincj glamping resort
+                    </p>
+                    <p className={s.date}>aug 2024 - mar 2025</p>
+                    <p className={s.task}>
+                      Designed and analyzed a full-stack booking system using
+                      Express.js and Next.js.
+                    </p>
+                    <p className={s.task}>
+                      Managed Git workflows, pull requests, and version control
+                      for clean and stable builds
+                    </p>
+                    <p className={s.task}>
+                      Deployed the frontend to Vercel and the API backend to
+                      DigitalOcean, ensuring system availability and scalability
+                    </p>
+                    <p className={s.task}>
+                      streamlined the customer booking and admin management
+                      process, reducing manual tasks
+                    </p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </article>
         </div>
       </main>
       <Footer />
